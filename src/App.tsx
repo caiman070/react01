1be61979d1ae87b7018ced3cace744e607e45dfd
wfruto <wfruto@cuc.edu.co>
2024-09-16 09:41:14 -0500

    proyecto react 01

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import milogo  from "./assets/img/logoReact.png"
+
+function App() {
+ 
+  return (
+    <>
+      <div>
+       <img src={milogo} alt="Logo de React" />
+       <h1>hola mundo</h1>
+       <p>primer proyecto  con react</p>
+      </div>
+    </>
+  )
+}
+
+export default App
